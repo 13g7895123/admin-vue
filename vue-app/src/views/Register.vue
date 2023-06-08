178798671a3a751140af29f38625c95e1e29bb48
Jarvis <13g7895123@gmail.com>
2023-06-08 22:40:58 +0800

    add register page

--- vue-app/src/views/Register.vue
+++ vue-app/src/views/Register.vue
@@ -2,7 +2,10 @@
     <div class="relative w-screen h-screen bg-registerBg bg-cover bg-no-repeat bg-center">
         <section class="w-[370px] h-[210px] absolute top-[10%] left-[34%] text-center">
             <sapn class="text-[26px]">後臺管理系統</sapn>
-            <el-form label-width="100px">
+            <el-form 
+            label-width="100px"
+            class="mt-5 bg-white py-5 px-3 rounded-md shadow-md"
+            >
                 <el-form-item label="使用者名稱" prop="name">
                     <el-input placeholder="請輸入使用者名稱"></el-input>
                 </el-form-item>
@@ -15,9 +18,9 @@
                 <el-form-item label="確認密碼" prop="passwordCheck">
                     <el-input placeholder="請確認密碼"></el-input>
                 </el-form-item>
-                <!-- <el-form-item label="選擇身分" prop="name">
-                    <el-select label>
-                </el-form-item> -->
+                <el-form-item>
+                    <el-button class="w-full">註冊</el-button>
+                </el-form-item>
             </el-form>
         </section>
     </div>
