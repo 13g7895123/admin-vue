45ea0f379fe35005e5e0da190507d2654c56f830
Jarvis <13g7895123@gmail.com>
2023-06-08 18:39:33 +0800

    test

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,31 +1,30 @@
-<script setup>
-import TaskPage from './pages/TaskPage.vue';
-import Navbar from './components/Navbar.vue';
-import Sidebar from './components/Sidebar.vue';
-import NavbarRight from './components/NavbarRight.vue';
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="flex overflow-hidden">
-    <!-- left -->
-    <Sidebar />
-    <!-- right -->
-    <div class="w-[calc(100%-15rem)] flex flex-col">
-      <!-- navbar -->
-      <NavbarRight />
-      <!-- content -->
-      <div class="h-[calc(100%-2.5rem)] flex justify-center items-center">
-        <router-view/>
-      </div>
-    </div>    
-  </div>  
-  <!-- <Navbar/> -->
-  <!-- <router-view v-slot="{ Component }" class="max-w-[1280px] mx-auto">
-    <transition name="fade" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view> -->
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
