e57dacfa662b5bf994a43d175b89fdbba33ccb3a
Jarvis <13g7895123@gmail.com>
2023-06-08 21:52:29 +0800

    install elemeny plus

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css
 
 // router
 import route from "./router"
@@ -9,6 +11,7 @@ import route from "./router"
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
+app.use(ElementPlus)
 app.use(route)
 app.use(createPinia())
 app.mount('#app')
