baa894140d6346f0e5f23e49c2e244649509e081
Jarvis <13g7895123@gmail.com>
2023-06-08 22:09:33 +0800

    add 404 gif

--- vue-app/src/views/404.vue
+++ vue-app/src/views/404.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="w-full h-screen overflow-hidden">
-        <img class="w-auto h-screen" src="../assets/404.gif" alt="error">
+        <img class="w-screen h-screen" src="../assets/404.gif" alt="error">
         <button @click="$router.push('/')">返回首頁</button>
     </div>
 </template>
+<script setup>
+
+</script>
