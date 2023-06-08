a60825ff5393f92449d37f5df8d9a772911308ca
Jarvis <13g7895123@gmail.com>
2023-06-08 22:32:55 +0800

    add register page

--- vue-app/src/views/Register.vue
+++ vue-app/src/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="relative w-screen h-screen bg-registerBg bg-no-repeat bg-center">
+    <div class="relative w-screen h-screen bg-registerBg bg-cover bg-no-repeat bg-center">
         <section class="w-[370px] h-[210px] absolute top-[10%] left-[34%] text-center">
             <sapn class="text-[26px]">後臺管理系統</sapn>
         </section>
