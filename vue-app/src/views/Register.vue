ff9e64955cbcd92137f540eccbbac6f37fc8eaf4
Jarvis <13g7895123@gmail.com>
2023-06-09 19:10:37 +0800

    add register page

--- vue-app/src/views/Register.vue
+++ vue-app/src/views/Register.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="relative w-screen h-screen bg-registerBg bg-cover bg-no-repeat bg-center">
         <section class="w-[370px] h-[210px] absolute top-[10%] left-[34%] text-center">
-            <sapn class="text-[26px]">後臺管理系統</sapn>
+            <span class="text-[26px]">後臺管理系統</span>
             <el-form 
             :rules="rules"
             :model="registerUser"
