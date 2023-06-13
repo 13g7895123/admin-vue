f3ae3f5c8dba4d04a3b688f49d49f829c1e4825b
Jarvis <13g7895123@gmail.com>
2023-06-13 22:55:21 +0800

    新增登入頁

--- vue-app/src/router/routes.js
+++ vue-app/src/router/routes.js
@@ -10,6 +10,11 @@ const routes = [
         component: () => import('../views/Register.vue'),
     },
     {
+        path: "/login",
+        name: 'Login',
+        component: () => import('../views/Login.vue'),
+    },
+    {
         path: '/:catchAll(.*)',
         name: '404',
         component: () => import('../views/404.vue')
