12bdb45dd702d42e83aae14096831b6e067186bb
Jarvis <13g7895123@gmail.com>
2023-06-24 21:27:12 +0800

    test sessionstorage

--- vue-app/src/views/Login.vue
+++ vue-app/src/views/Login.vue
@@ -60,7 +60,7 @@ const handleSubmit = (formEl) => {
     if (!formEl) return;
     formEl.validate(async(valid) => {
         if (valid){
-            const { data: { inner_data }} = await axios.post(
+            const { data: { data }} = await axios.post(
                 'http://139.162.15.125:9090/api/health-insurance/admin-login.php',
                 loginUser.value
             )
