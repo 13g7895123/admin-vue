f161093957323cf630aded9a3f7d1cf9cc1c5684
Jarvis <13g7895123@gmail.com>
2023-06-25 13:13:36 +0800

    add button

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -7,24 +7,42 @@
         border
         v-if="tableData.length > 0"
         >
-        <el-table-column type="index" label="編號" align="center" width="70" />
-        <el-table-column label="帳號" align="center" width="100" prop="account" />
-        <el-table-column
-            label="名稱"
-            align="center"
-            width="100"
-            prop="user_name"
-        />
-        <el-table-column
-            label="電話"
-            align="center"
-            width="100"
-            prop="phone"
-        />
-        <el-table-column label="信箱" align="center" width="auto" prop="mail" />
-        <el-table-column label="地址" align="center" width="auto" prop="address_country" />
-        <el-table-column label="機構代號" align="center" width="auto" prop="medical_institution_code" />
-        <el-table-column label="藥局名稱" align="center" width="auto" prop="medical_institution_name" />
+            <el-table-column type="index" label="編號" align="center" width="70" />
+            <el-table-column label="帳號" align="center" width="100" prop="account" />
+            <el-table-column
+                label="名稱"
+                align="center"
+                width="100"
+                prop="user_name"
+            />
+            <el-table-column
+                label="電話"
+                align="center"
+                width="100"
+                prop="phone"
+            />
+            <el-table-column label="信箱" align="center" width="auto" prop="mail" />
+            <el-table-column label="地址" align="center" width="auto" prop="address_country" />
+            <el-table-column label="機構代號" align="center" width="auto" prop="medical_institution_code" />
+            <el-table-column label="藥局名稱" align="center" width="auto" prop="medical_institution_name" />
+            <el-table-column
+                label="操作"
+                align="center"
+                width="auto"
+                prop="operation"
+            >
+                <template #default="scope">
+                <el-button size="small" type="primary" @click="handleEdit(scope.row)"
+                    >编辑</el-button
+                >
+                <el-button
+                    size="small"
+                    type="danger"
+                    @click="handleDelete(scope.$idnex, scope.row)"
+                    >删除</el-button
+                >
+                </template>
+            </el-table-column>
         </el-table>
     </div>
 </template>
