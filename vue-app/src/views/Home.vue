70d0ff211d5524d346ce0975c16703dc079a22ab
Jarvis <13g7895123@gmail.com>
2023-06-25 11:34:22 +0800

    add side bar

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,8 +1,10 @@
 <template>
     <Navbar />
+    <Sidebar />
     <div>初始化頁面</div>
 </template>
 <script setup>
 import Navbar from '../components/Navbar.vue'
+import Sidebar from '../components/Sidebar.vue';
 
 </script>
