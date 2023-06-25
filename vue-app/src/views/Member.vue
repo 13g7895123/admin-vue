7c85f624d922267566ba6be9747de1d3dca69cc7
Jarvis <13g7895123@gmail.com>
2023-06-25 12:39:11 +0800

    test api

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -8,7 +8,7 @@ import axios from 'axios';
 const tableData = ref([])
 
 const getMember = async() => {
-    const { data } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-member.php')
+    const { data: { success, data } } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-member.php')
     console.log(data);
 }
 
