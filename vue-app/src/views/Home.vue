5e94085bb55af5dd34bf273e7031310e8a6c9fb0
Jarvis <13g7895123@gmail.com>
2023-06-25 12:23:29 +0800

    debug

--- vue-app/src/views/Home.vue
+++ vue-app/src/views/Home.vue
@@ -1,10 +1,11 @@
 <template>
-    <Navbar />
-    <Sidebar />
-    <div class="relative top-0 left-[180px] w-[calc(100% - 180px)] h-[calc(100% - 71px)] overflow-auto">
-      <router-view></router-view>
+    <div class="w-full h-full overflow-hidden">
+        <Navbar />
+        <Sidebar />
+        <div class="relative top-0 left-[180px] w-[calc(100% - 180px)] h-[calc(100% - 71px)] overflow-auto">
+            <router-view></router-view>
+        </div>
     </div>
-    <!-- <div>初始化頁面</div> -->
 </template>
 <script setup>
 import Navbar from '../components/Navbar.vue'
