fc21b432543352a4902d2b92c09c15397a499799
Jarvis <13g7895123@gmail.com>
2023-06-25 18:13:31 +0800

    revise render

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -48,13 +48,13 @@
             <el-table-column
                 label="電話"
                 align="center"
-                width="auto"
+                width="200"
                 prop="phone"
             />
-            <el-table-column label="信箱" align="center" width="auto" prop="mail" />
-            <el-table-column label="地址" align="center" width="auto" prop="address_country" />
-            <el-table-column label="機構代號" align="center" width="auto" prop="medical_institution_code" />
-            <el-table-column label="藥局名稱" align="center" width="auto" prop="medical_institution_name" />
+            <el-table-column label="信箱" align="center" width="200" prop="mail" />
+            <el-table-column label="地址" align="center" width="200" prop="address_country" />
+            <el-table-column label="機構代號" align="center" width="200" prop="medical_institution_code" />
+            <el-table-column label="藥局名稱" align="center" width="200" prop="medical_institution_name" />
             
         </el-table>
     </div>
