b410da014a7a83ff0a9ca8c206844722658ca562
Jarvis <13g7895123@gmail.com>
2023-06-25 17:43:06 +0800

    debug

--- vue-app/src/components/DialogModel.vue
+++ vue-app/src/components/DialogModel.vue
@@ -3,15 +3,18 @@
         :before-close="handleClose"
         v-model="props.show"
         :title="editData ? '編輯會員資料' : '新增會員資料'">
-        
+
     </el-dialog>
 </template>
 <script setup>
 const props = defineProps({
-    show: Boolean
+    show: Boolean,
+    editData: Object
 })
 
 const handleClose = () => {
-
+    emits("closeModal");
 }
+
+const emits = defineEmits(["closeModal", "handleUpdateProfiles"]);
 </script>
