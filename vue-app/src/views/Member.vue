3569698795417a7c40879702bc6e9ee40eb3b591
Jarvis <13g7895123@gmail.com>
2023-06-25 18:08:38 +0800

    revise render

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -19,7 +19,7 @@
         <el-table-column
                 label="操作"
                 align="center"
-                width="auto"
+                width="70"
                 prop="operation"
             >
                 <template #default="scope">
