fd49464eafa2f94618c2cf3a6c32cb3635a8ae8e
Jarvis <13g7895123@gmail.com>
2023-06-25 12:47:12 +0800

    test tabledata

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -1,5 +1,30 @@
 <template>
-    這是會員管理頁面
+    <el-table
+      :data="tableData"
+      style="width: 95%"
+      max-height="450"
+      border
+      v-if="tableData.length > 0"
+    >
+      <el-table-column type="index" label="編號" align="center" width="70" />
+      <el-table-column label="帳號" align="center" width="auto" prop="account" />
+      <el-table-column
+        label="名稱"
+        align="center"
+        width="auto"
+        prop="user_name"
+      />
+      <el-table-column
+        label="電話"
+        align="center"
+        width="auto"
+        prop="phone"
+      />
+      <el-table-column label="信箱" align="center" width="auto" prop="mail" />
+      <el-table-column label="地址" align="center" width="auto" prop="address_country" />
+      <el-table-column label="機構代號" align="center" width="auto" prop="medical_institution_code" />
+      <el-table-column label="藥局名稱" align="center" width="auto" prop="medical_institution_name" />
+    </el-table>
 </template>
 <script setup>
 import { ref, watchEffect } from 'vue'
@@ -10,6 +35,7 @@ const tableData = ref([])
 const getMember = async() => {
     const { data: { success, data } } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-member.php')
     console.log(data);
+    tableData.value = data
 }
 
 watchEffect(() => getMember())
