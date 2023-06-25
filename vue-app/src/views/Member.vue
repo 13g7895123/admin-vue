2ba391825c0c960b926191d5c65d9b1d936f2d81
Jarvis <13g7895123@gmail.com>
2023-06-25 12:37:59 +0800

    member get data

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -1,3 +1,16 @@
 <template>
     這是會員管理頁面
 </template>
+<script setup>
+import { ref, watchEffect } from 'vue'
+import axios from 'axios';
+
+const tableData = ref([])
+
+const getMember = async() => {
+    const { data } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-member.php')
+    console.log(data);
+}
+
+watchEffect(() => getMember())
+</script>
