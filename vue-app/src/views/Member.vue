190dad3c4ccbb23d2fe0c3bcdd95f6dc3e83fa9e
Jarvis <13g7895123@gmail.com>
2023-06-27 23:34:51 +0800

    debug

--- vue-app/src/views/Member.vue
+++ vue-app/src/views/Member.vue
@@ -39,12 +39,12 @@
             </el-table-column>
             <el-table-column type="index" label="編號" align="center" width="100"/>
             <el-table-column label="帳號" align="center" width="200" prop="account"/>
-            <el-table-column label="名稱" align="center" width="200" prop="user_name"/>
+            <el-table-column label="聯絡人" align="center" width="200" prop="user_name"/>
             <el-table-column label="電話" align="center" width="200" prop="phone"/>
             <el-table-column label="信箱" align="center" width="200" prop="mail"/>
-            <el-table-column label="地址" align="center" width="200" prop="address1"/>
-            <el-table-column label="機構代號" align="center" width="200" prop="MICode"/>
-            <el-table-column label="藥局名稱" align="center" width="200" prop="MIName"/>
+            <el-table-column label="地址" align="center" width="200" prop="address_country"/>
+            <el-table-column label="機構代號" align="center" width="200" prop="medical_institution_code"/>
+            <el-table-column label="藥局名稱" align="center" width="200" prop="medical_institution_name"/>
         </el-table>
     </div>
     <DialogModel 
