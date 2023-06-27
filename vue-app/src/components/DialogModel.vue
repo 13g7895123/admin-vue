f67c597c1cbbb84f96c52e735bed9d5bf4bf38eb
Jarvis <13g7895123@gmail.com>
2023-06-27 23:12:01 +0800

    debug

--- vue-app/src/components/DialogModel.vue
+++ vue-app/src/components/DialogModel.vue
@@ -2,7 +2,7 @@
     <el-dialog 
         :before-close="handleClose"
         v-model="props.show"
-        :title="editData ? '編輯會員資料' : '新增會員資料'">
+        :title="props.editData ? '編輯會員資料' : '新增會員資料'">
         <el-form
             :model="dialogData"
             ref="dialogRef"
