db9245d6bc495e18818929f07e400e617769e51a
Jarvis <13g7895123@gmail.com>
2023-07-01 17:27:23 +0800

    add data to pinia

--- vue-app/src/stores/loginAuth.js
+++ vue-app/src/stores/loginAuth.js
@@ -2,22 +2,28 @@ import { defineStore } from "pinia";
 
 export const useAuthStore = defineStore('auth', {
     state: () => {
-        return { isAuthenticated: false, user: {} }
+        return { isAuthenticated: false, user: {}, userName: {} }
     },
     getters: {
         getAuthenticated: (state) => state.isAuthenticated,
         getUser: (state) => state.user,
+        getUserName: (state) => state.userName,
       },
       actions: {
         setAuth(isAuth){
             this.isAuthenticated = isAuth;            
         },
-        setUser(user , userName){
+        setUser(user){
             if(user){
                 this.user = user;
-                this.userName = userName;
             }else {
                 this.user = {}
+            }
+        },
+        setUserName(userName){
+            if(userName){
+                this.userName = userName;
+            }else {
                 this.userName = {}
             }
         },
