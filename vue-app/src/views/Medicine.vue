9b649324fb5fb931e67f688892d3042967580e7b
Jarvis <13g7895123@gmail.com>
2023-07-01 12:16:39 +0800

    調整欄位名稱

--- vue-app/src/views/Medicine.vue
+++ vue-app/src/views/Medicine.vue
@@ -1,61 +1,136 @@
 <template>
     <div class="h-full px-5 py-3">
+        <div>
+            <el-form :inline="true">
+                <el-form-item class="float-right">
+                    <el-button type="primary" size="small" @click="handleAdd"
+                        >新增</el-button
+                    >
+                </el-form-item>
+            </el-form>
+        </div>
         <el-table
         :data="tableData"
         max-height="450"
+        style="width: 100%"
         border
         v-if="tableData.length > 0"
         >
-            <el-table-column type="index" label="編號" align="center" width="70" />
-            <el-table-column label="藥品編號" align="center" width="100" prop="account" />
-            <el-table-column
-                label="英文名稱"
-                align="center"
-                width="100"
-                prop="user_name"
-            />
-            <el-table-column
-                label="電話"
-                align="center"
-                width="100"
-                prop="phone"
-            />
-            <el-table-column label="信箱" align="center" width="auto" prop="mail" />
-            <el-table-column label="地址" align="center" width="auto" prop="address_country" />
-            <el-table-column label="機構代號" align="center" width="auto" prop="medical_institution_code" />
-            <el-table-column label="藥局名稱" align="center" width="auto" prop="medical_institution_name" />
-            <el-table-column
+        <el-table-column
                 label="操作"
                 align="center"
-                width="auto"
+                width="120"
                 prop="operation"
             >
                 <template #default="scope">
-                <el-button size="small" type="primary" @click="handleEdit(scope.row)"
-                    >编辑</el-button
-                >
-                <el-button
-                    size="small"
-                    type="danger"
-                    @click="handleDelete(scope.$idnex, scope.row)"
-                    >删除</el-button
-                >
+                    <el-button
+                        size="small"
+                        type="primary"
+                        @click="handleEdit(scope.row)">
+                        <el-icon><EditPen /></el-icon>
+                    </el-button>
+                    <el-button
+                        size="small"
+                        type="danger"
+                        @click="handleDelete(scope.row)">
+                        <el-icon><Delete /></el-icon>
+                    </el-button>
                 </template>
             </el-table-column>
+            <el-table-column type="index" label="編號" align="center" width="100"/>
+            <el-table-column label="藥品編號" align="center" width="200" prop="medicine_code"/>
+            <el-table-column label="英文名" align="center" width="200" prop="eng_name"/>
+            <el-table-column label="中文名" align="center" width="200" prop="medicine_name"/>
+            <el-table-column label="成分" align="center" width="200" prop="ingredient"/>
+            <el-table-column label="數量" align="center" width="200" prop="ingredient_num"/>
+            <el-table-column label="特殊用量" align="center" width="200" prop="specification_quantity"/>
+            <el-table-column label="單複方" align="center" width="200" prop="single_compound"/>
+            <el-table-column label="價格" align="center" width="200" prop="price"/>
+            <el-table-column label="起訖日" align="center" width="200" prop="start_and_end"/>
+            <el-table-column label="製造公司" align="center" width="200" prop="medicine_manufacturer"/>
+            <el-table-column label="劑量類型" align="center" width="200" prop="dosage"/>
+            <el-table-column label="類別" align="center" width="200" prop="classification"/>
+            <el-table-column label="類別群組" align="center" width="200" prop="classification_group"/>
+            <el-table-column label="ATC編號" align="center" width="200" prop="ATC_code"/>
+            <el-table-column label="備註" align="center" width="200" prop="remark"/>
+            <el-table-column label="更新日期" align="center" width="200" prop="date_time_create"/>
         </el-table>
     </div>
+    <DialogModel 
+        :show="show"
+        :editData="editData"
+        :operation = 'operation'
+        @closeModel="closeModel"
+        @handleUpdateProfiles="handelUpdateMember"
+        />
 </template>
 <script setup>
 import { ref, watchEffect } from 'vue'
 import axios from 'axios';
+import { EditPen, Delete } from "@element-plus/icons-vue";
+import Swal from 'sweetalert2'
 
 const tableData = ref([])
+const show = ref(false)
+const editData = ref()
+const operation = ref()   // 0為編輯，1為新增
 
 const getMember = async() => {
-    const { data: { success, data } } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-member.php')
-    console.log(data);
-    tableData.value = data
+    const { data: { success, data } } = await axios.post('http://139.162.15.125:9090/api/health-insurance/admin-medicine.php')
+
+    if (success){
+        tableData.value = data
+    }else{
+        history.go(0)
+    }
 }
 
 watchEffect(() => getMember())
+
+const handleAdd = () => {
+    show.value = true
+    operation.value = true
+}
+
+const handleEdit = (row) => {
+    console.log('edit click');
+    show.value = true
+    editData.value = row
+    operation.value = false
+}
+
+const handleDelete = async(row) => {
+    const { account } = row
+    const ajax_data = {
+        account: account
+    }
+
+    const { data: { success, msg } } = await axios.post(
+        `http://139.162.15.125:9090/api/health-insurance/admin-member-delete.php`,
+        ajax_data
+    )
+    console.log(success);
+
+    if (success){
+        Swal.fire({
+            title: `刪除會員成功`,
+            icon: 'success',
+            showConfirmButton: false,
+            showCancelButton: false,
+            timer: 2000,
+        }).then(() => {
+            handelUpdateMember()
+        })
+    }
+}
+
+const handelUpdateMember = () => {
+    getMember()
+}
+
+const closeModel = () => {
+    show.value = false
+    editData.value = {}
+}
+
 </script>
